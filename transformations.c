a64224e18c4bbad653fa7bc5a752176612a92503
Erwan_Lbp <erwan.lbp@gmail.com>
2015-10-12 11:56:40 +0200

    corrige negatif et binarisation

--- transformations.c
+++ transformations.c
@@ -20,15 +20,15 @@ int binarisation (IMAGE *imageATransfo){
 				//Si la valeur est en dessous du seuil il prend la valeur 0
 				if (imageATransfo->mat[lig][col].r < seuil)
 				{
-					imageATransfo->mat[lig][col].r = 0;
-					imageATransfo->mat[lig][col].g = 0;
-					imageATransfo->mat[lig][col].b = 0;
-				}
-				//Si la valeur est en dessous du seuil il prend la valeur 1
-				else {
 					imageATransfo->mat[lig][col].r = 1;
 					imageATransfo->mat[lig][col].g = 1;
 					imageATransfo->mat[lig][col].b = 1;
+				}
+				//Si la valeur est en dessous du seuil il prend la valeur 1
+				else {
+					imageATransfo->mat[lig][col].r = 0;
+					imageATransfo->mat[lig][col].g = 0;
+					imageATransfo->mat[lig][col].b = 0;
 				}
 			}
 			
@@ -43,16 +43,26 @@ int binarisation (IMAGE *imageATransfo){
 	return 1;
 }
 
+
+// MARCHE PAS POUR LES BITMAPS
+// SI BITMAP : 1 - VALEUR.R
 int negatif(IMAGE *image){
 	int lig,col;
 
 	//Double boucle pour parcourir le tableau
 	for(lig=0;lig<image->nb_lig;lig++){
 		for(col=0;col<image->nb_col;col++){
-			//On applique la formule pour changer la valeur de chaque composante
-			image->mat[lig][col].r = 255 - (image->mat[lig][col].r);
-			image->mat[lig][col].g = 255 - (image->mat[lig][col].g);
-			image->mat[lig][col].b = 255 - (image->mat[lig][col].b);
+			//On applique la formule pour changer la valeur de chaque composante selon le type de fichier
+			if(image->type == 1 || image->type == 4){
+				image->mat[lig][col].r = 1 - (image->mat[lig][col].r);
+				image->mat[lig][col].g = 1 - (image->mat[lig][col].g);
+				image->mat[lig][col].b = 1 - (image->mat[lig][col].b);
+			}
+			else{
+				image->mat[lig][col].r = 255 - (image->mat[lig][col].r);
+				image->mat[lig][col].g = 255 - (image->mat[lig][col].g);
+				image->mat[lig][col].b = 255 - (image->mat[lig][col].b);
+			}
 		}
 	}
 	return 1;
@@ -203,25 +213,25 @@ int redimensionnement(IMAGE *image, const int argc, const char *argv[]){
 
 
 	//Copie des valeurs de image dans copieImage
-	for(lig=0;lig<copieImage.nb_lig;lig++){
-		for(col=0;col<copieImage.nb_col;col++){
-			copieImage.mat[lig][col]=image->mat[lig][col];
+		for(lig=0;lig<copieImage.nb_lig;lig++){
+			for(col=0;col<copieImage.nb_col;col++){
+				copieImage.mat[lig][col]=image->mat[lig][col];
+			}
 		}
-	}
 
 
 	//Liberation de la memoire de l'image reçue
-	vider_tab_pixels(image);
+		vider_tab_pixels(image);
 	//Reallocation d'un nouveau tableau de taille reduite a image
-	image->mat=NULL;
-	image->nb_lig = ordSortie-ordEntree;
-	image->nb_col = absSortie-absEntree;
-	image->type = copieImage.type;
-	image->max_val = copieImage.max_val;
-	
+		image->mat=NULL;
+		image->nb_lig = ordSortie-ordEntree;
+		image->nb_col = absSortie-absEntree;
+		image->type = copieImage.type;
+		image->max_val = copieImage.max_val;
 
 
-	alloc_tableau(image);
+
+		alloc_tableau(image);
 	/*image->mat = malloc((image->nb_lig)* sizeof(PIXEL));
 
 	//Verification de l'allocation de la premiere dimension du nouveau tableau
@@ -246,131 +256,131 @@ int redimensionnement(IMAGE *image, const int argc, const char *argv[]){
 
 
 	// Affectation des valeurs de copieImage dans image
-	for(lig=ordEntree; lig<=ordSortie; lig++){
-		for(col=absEntree; col<=absSortie;col++){
-			image->mat[lig-ordEntree][col-absEntree] = copieImage.mat[lig][col];
+		for(lig=ordEntree; lig<=ordSortie; lig++){
+			for(col=absEntree; col<=absSortie;col++){
+				image->mat[lig-ordEntree][col-absEntree] = copieImage.mat[lig][col];
+			}
 		}
-	}
 
 	//Vidage copieImage
-	vider_tab_pixels(&copieImage);
-	return 1;
-}
-
-int amelioration_du_contraste (IMAGE *imageATransfo){
-	//Si l'image est en couleur il y a besoin de la passer en niveau de gris
-	if(imageATransfo->type == 3 || imageATransfo->type == 6){
-		niveauGris(imageATransfo);
+		vider_tab_pixels(&copieImage);
+		return 1;
 	}
 
-	if(imageATransfo->type != 1 && imageATransfo->type != 4){
-		int lig,col, min = imageATransfo->mat[0][0].r, max = imageATransfo->mat[0][0].r;
-		
-		//Recherche du Min et du Max
-		for (lig = 0; lig < imageATransfo->nb_lig; lig++)
-		{
-			for (col = 0; col < imageATransfo->nb_col; col++)
-			{
-				if (imageATransfo->mat[lig][col].r < min)
-					min = imageATransfo->mat[lig][col].r;
-				if (imageATransfo->mat[lig][col].r > max)
-					max = imageATransfo->mat[lig][col].r;			
-			}
+	int amelioration_du_contraste (IMAGE *imageATransfo){
+	//Si l'image est en couleur il y a besoin de la passer en niveau de gris
+		if(imageATransfo->type == 3 || imageATransfo->type == 6){
+			niveauGris(imageATransfo);
 		}
+
+		if(imageATransfo->type != 1 && imageATransfo->type != 4){
+			int lig,col, min = imageATransfo->mat[0][0].r, max = imageATransfo->mat[0][0].r;
+
+		//Recherche du Min et du Max
+			for (lig = 0; lig < imageATransfo->nb_lig; lig++)
+			{
+				for (col = 0; col < imageATransfo->nb_col; col++)
+				{
+					if (imageATransfo->mat[lig][col].r < min)
+						min = imageATransfo->mat[lig][col].r;
+					if (imageATransfo->mat[lig][col].r > max)
+						max = imageATransfo->mat[lig][col].r;			
+				}
+			}
 
 		// On acoluste les valeurs sur l'echelle [0;max]
+			for (lig = 0; lig < imageATransfo->nb_lig; lig++)
+			{
+				for (col = 0; col < imageATransfo->nb_col; col++)
+				{
+				//On change toutes les composantes avec la formules trouvés dans l'etude theorique
+					imageATransfo->mat[lig][col].r = (imageATransfo->max_val * (imageATransfo->mat[lig][col].r - min))/(max-min);
+					imageATransfo->mat[lig][col].g = (imageATransfo->max_val * (imageATransfo->mat[lig][col].g - min))/(max-min);
+					imageATransfo->mat[lig][col].b = (imageATransfo->max_val * (imageATransfo->mat[lig][col].b - min))/(max-min);
+				}
+			}
+		// On change le type de fichier d'ecriture, pour economiser de la memoire
+			if(imageATransfo->type <= 3)
+				imageATransfo->type = 2;
+			else 
+				imageATransfo->type = 5;
+		}
+
+		return 1;
+	}
+
+	int lissage (IMAGE *imageATransfo){
+		int lig, col;
+		IMAGE copie;
+		copie.nb_lig = imageATransfo->nb_lig;
+		copie.nb_col = imageATransfo->nb_col;
+
+	//On créer le masque, une matrice 3x3 contenant que des 1
+		int *masque=NULL;
+		masque = malloc(9 * sizeof(int));
+		creation_masque(masque,1,1,1,1,1,1,1,1,1);
+
+	//On copie l'image de base dans la copie allouee dynamiquement
+		alloc_tableau(&copie);
 		for (lig = 0; lig < imageATransfo->nb_lig; lig++)
 		{
 			for (col = 0; col < imageATransfo->nb_col; col++)
 			{
-				//On change toutes les composantes avec la formules trouvés dans l'etude theorique
-				imageATransfo->mat[lig][col].r = (imageATransfo->max_val * (imageATransfo->mat[lig][col].r - min))/(max-min);
-				imageATransfo->mat[lig][col].g = (imageATransfo->max_val * (imageATransfo->mat[lig][col].g - min))/(max-min);
-				imageATransfo->mat[lig][col].b = (imageATransfo->max_val * (imageATransfo->mat[lig][col].b - min))/(max-min);
+				copie.mat[lig][col] = imageATransfo->mat[lig][col];
 			}
 		}
-		// On change le type de fichier d'ecriture, pour economiser de la memoire
-		if(imageATransfo->type <= 3)
-			imageATransfo->type = 2;
-		else 
-			imageATransfo->type = 5;
-	}
-
-	return 1;
-}
-
-int lissage (IMAGE *imageATransfo){
-	int lig, col;
-	IMAGE copie;
-	copie.nb_lig = imageATransfo->nb_lig;
-	copie.nb_col = imageATransfo->nb_col;
-	
-	//On créer le masque, une matrice 3x3 contenant que des 1
-	int *masque=NULL;
-	masque = malloc(9 * sizeof(int));
-	creation_masque(masque,1,1,1,1,1,1,1,1,1);
-
-	//On copie l'image de base dans la copie allouee dynamiquement
-	alloc_tableau(&copie);
-	for (lig = 0; lig < imageATransfo->nb_lig; lig++)
-	{
-		for (col = 0; col < imageATransfo->nb_col; col++)
-		{
-			copie.mat[lig][col] = imageATransfo->mat[lig][col];
-		}
-	}
 
 	//On commence les choses serieuses on fait le lissage
-	application_masque(imageATransfo, &copie, masque, 9);
-	
-	vider_tab_pixels(&copie);
+		application_masque(imageATransfo, &copie, masque, 9);
 
-	return 1;
-} 
+		vider_tab_pixels(&copie);
 
-int laplacien (IMAGE *imageATransfo){
-	int lig, col;
-	IMAGE copie;
-	copie.nb_lig = imageATransfo->nb_lig;
-	copie.nb_col = imageATransfo->nb_col;
+		return 1;
+	} 
 
-	alloc_tableau(&copie);
+	int laplacien (IMAGE *imageATransfo){
+		int lig, col;
+		IMAGE copie;
+		copie.nb_lig = imageATransfo->nb_lig;
+		copie.nb_col = imageATransfo->nb_col;
+
+		alloc_tableau(&copie);
 
 	//On copie l'image de base dans la copie allouee dynamiquement
-	for (lig = 0; lig < imageATransfo->nb_lig; lig++)
-	{
-		for (col = 0; col < imageATransfo->nb_col; col++)
+		for (lig = 0; lig < imageATransfo->nb_lig; lig++)
 		{
-			copie.mat[lig][col] = imageATransfo->mat[lig][col];
+			for (col = 0; col < imageATransfo->nb_col; col++)
+			{
+				copie.mat[lig][col] = imageATransfo->mat[lig][col];
+			}
 		}
-	}
 
 	//On commence les choses serieuses on fait le laplacien
-	for (lig = 1; lig < imageATransfo->nb_lig - 2; lig++)
-	{
-		for (col = 1; col < imageATransfo->nb_col - 2; col++)
+		for (lig = 1; lig < imageATransfo->nb_lig - 2; lig++)
 		{
+			for (col = 1; col < imageATransfo->nb_col - 2; col++)
+			{
 			imageATransfo->mat[lig][col].r = (  //1ere ligne
-												(imageATransfo->mat[lig-1][col].r)+
+				(imageATransfo->mat[lig-1][col].r)+
 												//2eme ligne
-												(imageATransfo->mat[lig][col-1].r) + (imageATransfo->mat[lig][col].r)*(-4) + (imageATransfo->mat[lig][col+1].r)+
+				(imageATransfo->mat[lig][col-1].r) + (imageATransfo->mat[lig][col].r)*(-4) + (imageATransfo->mat[lig][col+1].r)+
 												//3eme ligne
-												(imageATransfo->mat[lig+1][col].r));
+				(imageATransfo->mat[lig+1][col].r));
 			printf("%d\n", imageATransfo->mat[lig][col].r);
 
 			imageATransfo->mat[lig][col].g = (  //1ere ligne
-												(imageATransfo->mat[lig-1][col].g)+
+				(imageATransfo->mat[lig-1][col].g)+
 												//2eme ligne
-												(imageATransfo->mat[lig][col-1].g) + (imageATransfo->mat[lig][col].g)*(-4) + (imageATransfo->mat[lig][col+1].g)+
+				(imageATransfo->mat[lig][col-1].g) + (imageATransfo->mat[lig][col].g)*(-4) + (imageATransfo->mat[lig][col+1].g)+
 												//3eme ligne
-												(imageATransfo->mat[lig+1][col].g));
+				(imageATransfo->mat[lig+1][col].g));
 
 			imageATransfo->mat[lig][col].b = (  //1ere ligne
-												(imageATransfo->mat[lig-1][col].b)+
+				(imageATransfo->mat[lig-1][col].b)+
 												//2eme ligne
-												(imageATransfo->mat[lig][col-1].b) + (imageATransfo->mat[lig][col].b)*(-4) + (imageATransfo->mat[lig][col+1].b)+
+				(imageATransfo->mat[lig][col-1].b) + (imageATransfo->mat[lig][col].b)*(-4) + (imageATransfo->mat[lig][col+1].b)+
 												//3eme ligne
-												(imageATransfo->mat[lig+1][col].b));
+				(imageATransfo->mat[lig+1][col].b));
 		}
 	}
 
@@ -439,25 +449,25 @@ void application_masque (IMAGE *image, IMAGE *copie, int *masque, int diviseur){
 		{
 
 			image->mat[lig][col].r = (  //1ere ligne
-												(copie->mat[lig-1][col-1].r*masque[0])+(copie->mat[lig-1][col].r*masque[1])+(copie->mat[lig-1][col+1].r)*masque[2]+
+				(copie->mat[lig-1][col-1].r*masque[0])+(copie->mat[lig-1][col].r*masque[1])+(copie->mat[lig-1][col+1].r)*masque[2]+
 												//2eme ligne
-												(copie->mat[lig][col-1].r*masque[3])+(copie->mat[lig][col].r*masque[4])+(copie->mat[lig][col+1].r*masque[5])+
+				(copie->mat[lig][col-1].r*masque[3])+(copie->mat[lig][col].r*masque[4])+(copie->mat[lig][col+1].r*masque[5])+
 												//3eme ligne
-												(copie->mat[lig+1][col-1].r*masque[6])+(copie->mat[lig+1][col].r*masque[7])+(copie->mat[lig+1][col+1].r*masque[8]))/ diviseur;
+				(copie->mat[lig+1][col-1].r*masque[6])+(copie->mat[lig+1][col].r*masque[7])+(copie->mat[lig+1][col+1].r*masque[8]))/ diviseur;
 
 			image->mat[lig][col].g = (  //1ere ligne
-												(copie->mat[lig-1][col-1].g*masque[0])+(copie->mat[lig-1][col].g*masque[1])+(copie->mat[lig-1][col+1].g*masque[2])+
+				(copie->mat[lig-1][col-1].g*masque[0])+(copie->mat[lig-1][col].g*masque[1])+(copie->mat[lig-1][col+1].g*masque[2])+
 												//2eme ligne
-												(copie->mat[lig][col-1].g*masque[3])+(copie->mat[lig][col].g*masque[4])+(copie->mat[lig][col+1].g*masque[5])+
+				(copie->mat[lig][col-1].g*masque[3])+(copie->mat[lig][col].g*masque[4])+(copie->mat[lig][col+1].g*masque[5])+
 												//3eme ligne
-												(copie->mat[lig+1][col-1].g*masque[6])+(copie->mat[lig+1][col].g*masque[7])+(copie->mat[lig+1][col+1].g*masque[8]))/ diviseur;
+				(copie->mat[lig+1][col-1].g*masque[6])+(copie->mat[lig+1][col].g*masque[7])+(copie->mat[lig+1][col+1].g*masque[8]))/ diviseur;
 
 			image->mat[lig][col].b = (  //1ere ligne
-												(copie->mat[lig-1][col-1].b*masque[0])+(copie->mat[lig-1][col].b*masque[1])+(copie->mat[lig-1][col+1].b*masque[2])+
+				(copie->mat[lig-1][col-1].b*masque[0])+(copie->mat[lig-1][col].b*masque[1])+(copie->mat[lig-1][col+1].b*masque[2])+
 												//2eme ligne
-												(copie->mat[lig][col-1].b*masque[3])+(copie->mat[lig][col].b*masque[4])+(copie->mat[lig][col+1].b*masque[5])+
+				(copie->mat[lig][col-1].b*masque[3])+(copie->mat[lig][col].b*masque[4])+(copie->mat[lig][col+1].b*masque[5])+
 												//3eme ligne
-												(copie->mat[lig+1][col-1].b*masque[6])+(copie->mat[lig+1][col].b*masque[7])+(copie->mat[lig+1][col+1].b*masque[8]))/ diviseur;
+				(copie->mat[lig+1][col-1].b*masque[6])+(copie->mat[lig+1][col].b*masque[7])+(copie->mat[lig+1][col+1].b*masque[8]))/ diviseur;
 		}
 	}
 
