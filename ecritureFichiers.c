22151a8d604dc3914c9e892e962449614f13197b
Erwan_Lbp <erwan.lbp@gmail.com>
2015-10-14 11:25:55 +0200

    Corrige la fonction ecriture P5

--- ecritureFichiers.c
+++ ecritureFichiers.c
@@ -135,7 +135,7 @@ int ecriture_P4(FILE* fichier, const IMAGE * tab_pixels, const char nomImage[255
 
 int ecriture_P5(FILE* fichier, const IMAGE * tab_pixels, const char nomImage[255], const char transformation[100]){
 
-	fprintf(fichier, "P5\n%d %d\n%d", tab_pixels->nb_col, tab_pixels->nb_lig, tab_pixels->max_val); 
+	fprintf(fichier, "P5\n%d %d\n%d\n", tab_pixels->nb_col, tab_pixels->nb_lig, tab_pixels->max_val); 
 	
 	int lig,col;
 	for(lig=0; lig<tab_pixels->nb_lig; lig++){
